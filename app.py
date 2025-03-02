feafe7a72f5021322bce07ab52dfa10f3ad39d38
Wuuzzaa <jonas-licht@gmx.de>
2025-03-02 20:26:44 +0100

    add spreads.py.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.set_page_config(layout="wide")
 # Titel
 st.title("SKULD - Option Viewer")
 
+
 # load dataframe
 @st.cache_data
 def load_dataframe():
@@ -20,12 +21,12 @@ if 'df' not in st.session_state:
     st.success(f"File loaded successfully: {PATH_DATAFRAME_DATA_MERGED_FEATHER}")
 
 # Define pages
-gesamter_dataframe = st.Page("pages/total_dataframe.py", title="Total Data")
-gefilterte_ansicht = st.Page("pages/filtered_dataframe.py", title="Filtered Data")
+total_dataframe = st.Page("pages/total_dataframe.py", title="Total Data")
+filtered_dataframe = st.Page("pages/filtered_dataframe.py", title="Filtered Data")
 analyst_prices = st.Page("pages/analyst_prices.py", title="Analyst Prices")
 
 # Set up navigation
-page = st.navigation([gesamter_dataframe, gefilterte_ansicht, analyst_prices])
+page = st.navigation([total_dataframe, filtered_dataframe, analyst_prices])
 
 # Run the selected page
 page.run()
