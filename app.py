53dd25ab15613551c9a25cc799b01ac87f2d2907
Wuuzzaa <jonas-licht@gmx.de>
2025-03-09 20:21:44 +0100

    should run online

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.title("SKULD - Option Viewer")
 # load dataframe
 @st.cache_data
 def load_dataframe():
-    #df = load_updated_data()
-    df = pd.read_feather(PATH_DATAFRAME_DATA_MERGED_FEATHER)
+    df = load_updated_data()
+    #df = pd.read_feather(PATH_DATAFRAME_DATA_MERGED_FEATHER) # run without google file up-download
     return df
 
 
