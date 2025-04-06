a9efddc82d04c5eca395a8a3fc6793887c7fe695
Wuuzzaa <jonas-licht@gmx.de>
2025-04-06 16:19:36 +0200

    add collect_symbols.py and make the needed changes in the config.py

--- src/collect_symbols.py
+++ src/collect_symbols.py
@@ -1,14 +1,20 @@
 import io
-import time
 import pandas as pd
 import requests
-
-from config import SYMBOLS
-
+import re
 import yfinance as yf
-
+from config import PATH_SYMBOLS_EXCHANGE_FILE
 from src.dividend_radar import download_xlsx_file
-from src.symbols_exchange import SYMBOLS_EXCHANGE
+
+
+def get_tastytrade_symbols():
+    URL_TASTY_TRADE_LIST = "https://finance.yahoo.com/quotes/AA,AAL,AAPL,AES,AMD,AMZN,AVGO,BA,BABA,BIDU,BYND,C,CAT,COF,COST,CRM,CRON,CRWD,CSCO,DIA,DIS,EEM,EWZ,FL,FXI,GDX,GDXJ,GE,GILD,GLD,GM,GOOG,GS,HAL,HD,HPE,IBM,IWM,JPM,LOW,M,MCD,META,MMM,MRVL,MSFT,MU,NFLX,NIO,NVDA,ORCL,PG,QCOM,QQQ,ROKU,SBUX,SHOP,SLV,SMH,SNAP,SPY,TGT,TLT,TSLA,UBER,USO,V,WBA,WFC,WMT,X,XBI,XLU,XOM,JD,XOP/"
+
+    match = re.search(r"quotes/([^/]+)", URL_TASTY_TRADE_LIST)
+    if match:
+        tastytradesymbols = match.group(1).split(',')
+
+    return tastytradesymbols
 
 
 def get_stock_exchange(symbol):
@@ -35,7 +41,6 @@ def get_most_active_symbols(n_most=250):
 
     # symbol names list
     symbols = [item["symbol"] for item in data["finance"]["result"][0]["quotes"]]
-    # exchanges = [item["exchange"] for item in data["finance"]["result"][0]["quotes"]]
 
     return symbols
 
@@ -45,15 +50,16 @@ def get_symbols_exchange_dict(symbols):
     symbols_not_on_yahoo_finance = {"BXS", "SQ"}
     symbols = sorted(list(set(symbols) - symbols_not_on_yahoo_finance))
 
-    # dividenradar uses . yahoo - for b-stocks
     symbols_string = " ".join(symbols)
-    symbols_string = symbols_string.replace("ARTN.A", "ARTNA")
-    symbols_string = symbols_string.replace("DGIC.A", "DGICA")
-    symbols_string = symbols_string.replace("DGIC.B", "DGICB")
-    symbols_string = symbols_string.replace("FCNC.A", "FCNCA")
-    symbols_string = symbols_string.replace("RBCA.A", "RBCAA")
-    symbols_string = symbols_string.replace("RUSH.A", "RUSHA")
-    symbols_string = symbols_string.replace(".", "-")
+
+    # neeed some adjustments dividenradar uses . yahoo - for b-stocks???
+    # symbols_string = symbols_string.replace("ARTN.A", "ARTNA")
+    # symbols_string = symbols_string.replace("DGIC.A", "DGICA")
+    # symbols_string = symbols_string.replace("DGIC.B", "DGICB")
+    # symbols_string = symbols_string.replace("FCNC.A", "FCNCA")
+    # symbols_string = symbols_string.replace("RBCA.A", "RBCAA")
+    # symbols_string = symbols_string.replace("RUSH.A", "RUSHA")
+    # symbols_string = symbols_string.replace(".", "-")
 
     # request all symbols
     tickers = yf.Tickers(symbols_string)
@@ -61,10 +67,13 @@ def get_symbols_exchange_dict(symbols):
     symbols_exchange_dict = dict()
 
     for symbol in tickers.tickers:
-        print(symbol)
-        exchange = tickers.tickers[symbol].fast_info.exchange
-        print(f"{symbol}: {exchange}")
-        symbols_exchange_dict[symbol] = exchange
+        try:
+            exchange = tickers.tickers[symbol].fast_info.exchange
+            print(f"{symbol}: {exchange}")
+            symbols_exchange_dict[symbol] = exchange
+        except KeyError as e:
+            error = f"Exception with symbol: {symbol}: {e}"
+            print(error)
 
     symbols_exchange_dict = yahoo_exchanges_2_tradingview(symbols_exchange_dict)
 
@@ -91,67 +100,15 @@ def yahoo_exchanges_2_tradingview(symbols_exchange_dict):
     return new_symbols_exchange_dict
 
 
-# def get_all_exchanges_for_symbols(symbols):
-#     """
-#     Yahoo Finance API Rate Limits:
-#     https://help.yahooinc.com/dsp-api/docs/rate-limits
-#
-#     :param symbols: list of symbols
-#     :return: dict key: symbol, value: exchange
-#     """
-#     yahoo_2_tradingview_exchange = {
-#         'NYQ': 'NYSE',
-#         'NMS': 'NASDAQ',
-#         'NCM': 'NASDAQ',  # probably wrong NASDAQ Capital Market.
-#         'NGM': 'NASDAQ',  # probably wrong Nasdaq Global Market
-#         'ASE': 'AMEX',
-#         'PCX': 'NYSE'  # probably wrong Pacific Exchange
-#     }
-#
-#     # DEBUG
-#     # raw_exchanges = {}
-#     #
-#     # for symbol in symbols:
-#     #     try:
-#     #         print(f"request: {symbol}")
-#     #         exchange_dict[symbol] = get_stock_exchange(symbol)
-#     #     except Exception as e:
-#     #         print(f"Fehler bei {symbol}: {e}")
-#     #
-#     #     time.sleep(1)
-#
-#     # DEBUG
-#     raw_exchanges = SYMBOLS_EXCHANGE
-#
-#     exchange_dict = {}
-#
-#     # map the exchanges
-#     for symbol, exchange in raw_exchanges.items():
-#         mapped_exchange = yahoo_2_tradingview_exchange.get(exchange)
-#         exchange_dict[symbol] = mapped_exchange
-#
-#     return exchange_dict
-
-
 if __name__ == '__main__':
-    """
-    PLAN:
-    - download dividendradar excel file
-    - download tastytrade list
-    - merge both
-    - get the exchange for each symbol with yf
-    - map the exchangenames from yf to tradingview
-    - store the symbols and exchange dict to symbols_exchange.py
-    - in the config read from this file
-    """
+    #     """
+    #     Yahoo Finance API Rate Limits:
+    #     https://help.yahooinc.com/dsp-api/docs/rate-limits
 
     # merge dividend radar with tastytrade list and the most active stocks according to yahoo finance
-    tastytrade_symbols = SYMBOLS
+    tastytrade_symbols = get_tastytrade_symbols()
     most_active_symbols = get_most_active_symbols()
-
-    # debug daten wieder laden.
     dividendradar_symbols = load_dividendradar_symbols()
-    #dividendradar_symbols = SYMBOLS_EXCHANGE.keys()
 
     print(f"tastytrade_symbols: {len(tastytrade_symbols)}")
     print(f"most_active_symbols: {len(most_active_symbols)}")
@@ -159,14 +116,14 @@ if __name__ == '__main__':
 
     symbols = sorted(list(set(dividendradar_symbols) | set(tastytrade_symbols) | set(most_active_symbols)))
 
-    print(f"symbols after merge: {len(symbols)}")
+    # remove symbols with '.'
+    # todo make this run with symbols containing a "."
+    symbols = [s for s in symbols if '.' not in s]
 
+    print(f"symbols after merge(without '.' in symbolname): {len(symbols)}")
 
     symbols_exchange = get_symbols_exchange_dict(symbols)
-    pass
 
-
-
-
-
-
+    # store the symbol exchange data
+    df = pd.DataFrame(list(symbols_exchange.items()), columns=['symbol', 'exchange'])
+    df.to_excel(PATH_SYMBOLS_EXCHANGE_FILE, index=False)
