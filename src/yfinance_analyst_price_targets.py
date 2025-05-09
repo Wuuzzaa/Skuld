c861255f06d84557d33692b3baaba06ea4684158
Wuuzzaa <jonas-licht@gmx.de>
2025-05-09 21:38:52 +0200

    BUGFIX: add handling for index symbols. They just do not have any predictions which results in 404 http errors.

--- src/yfinance_analyst_price_targets.py
+++ src/yfinance_analyst_price_targets.py
@@ -18,13 +18,18 @@ def scrape_yahoo_finance_analyst_price_targets(symbols):
         print(f"Scraping {symbol} on Yahoo Finance...")
         data = yf.Ticker(symbol)
 
-        # Get mean or set None if the yahoo finance have no data. mostly no data for index of ressources
-        mean_target = data.analyst_price_targets.get("mean", None)
+        # Get mean or set None if the yahoo finance has no data.
+        # Mostly no data for index of resources
+        try:
+            mean_target = data.analyst_price_targets.get("mean", None)
+        except Exception as e:
+            print(f"Error getting price target for {symbol}: {e}")
+            mean_target = None
 
         results.append({"symbol": symbol, "analyst_mean_target": mean_target})
 
         # 1 request per second api rate limit
-        time.sleep(1)
+        # time.sleep(1)
 
     df = pd.DataFrame(results)
     df.to_feather(PATH_DATAFRAME_DATA_ANALYST_PRICE_TARGET_FEATHER)
