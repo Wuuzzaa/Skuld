e595f1efb251e3f9735bde25f33e36356e3943ad
Wuuzzaa <jonas-licht@gmx.de>
2025-05-09 15:31:46 +0200

    iron condor speedup done

--- src/iron_condor_calculation.py
+++ src/iron_condor_calculation.py
@@ -1,59 +1,87 @@
 import pandas as pd
 from config import *
-from src.spreads_calculation import get_spreads, calc_JMS
-
-
-def __create_iron_condor(put_spread, call_spread):
-    iron_condor = pd.Series(data={
-
-        # same values for put and call spread
-        "symbol": put_spread["symbol"],
-        "epiration_date": put_spread["expiration_date"],
-
-        # call spread
-        "call_spread_sell_strike": call_spread["sell_strike"],
-        "call_spread_sell_price": call_spread["sell_price"],
-        "call_spread_sell_delta": call_spread["sell_delta"],
-        "call_spread_buy_strike": call_spread["buy_strike"],
-        "call_spread_buy_price": call_spread["buy_price"],
-        "call_spread_buy_delta": call_spread["buy_delta"],
-
-        # put spread
-        "put_spread_sell_strike": put_spread["sell_strike"],
-        "put_spread_sell_price": put_spread["sell_price"],
-        "put_spread_sell_delta": put_spread["sell_delta"],
-        "put_spread_buy_strike": put_spread["buy_strike"],
-        "put_spread_buy_price": put_spread["buy_price"],
-        "put_spread_buy_delta": put_spread["buy_delta"],
-
-        # iron condor
-        "max_profit": call_spread["max_profit"] + put_spread["max_profit"],
-        "theta": call_spread["spread_theta"] + put_spread["spread_theta"],
-        # max width *100 - total premium for the spreads
-        "bpr": max(call_spread["spread_width"], put_spread["spread_width"]) * 100 - (put_spread["max_profit"] + call_spread["max_profit"]),
-    })
-
-    # add JMS
-    iron_condor["jms"] = calc_JMS(iron_condor, is_iron_condor=True)
-
-    return iron_condor
+from src.spreads_calculation import get_spreads
 
 
 def get_iron_condors(df, expiration_date, delta_target, spread_width):
     spreads_df = get_spreads(df, expiration_date, delta_target, spread_width)
 
-    iron_condors = []
+    # separate call and put spreads in own dataframes
+    puts = spreads_df[spreads_df['option_type'] == 'put'].copy()
+    calls = spreads_df[spreads_df['option_type'] == 'call'].copy()
 
-    for symbol in spreads_df["symbol"].unique():
-        # put and call spread for the symbol
-        put_spread = spreads_df[(spreads_df["symbol"] == symbol) & (spreads_df["option_type"] == "put")].squeeze()
-        call_spread = spreads_df[(spreads_df["symbol"] == symbol) & (spreads_df["option_type"] == "call")].squeeze()
+    # set the same index on both based on the symbol.
+    # Each df has exactly one line of each symbol, so it matches.
+    puts.set_index('symbol', inplace=True)
+    calls.set_index('symbol', inplace=True)
 
-        # make an iron condor out of the two spreads
-        iron_condor = __create_iron_condor(put_spread, call_spread)
-        iron_condors.append(iron_condor)
+    # rename columns for Merge
+    puts = puts.rename(columns=lambda x: f"put_{x}" if x != "symbol" else x)
+    calls = calls.rename(columns=lambda x: f"call_{x}" if x != "symbol" else x)
 
-    return pd.DataFrame(iron_condors)
+    # merge via symbol
+    iron_condors = pd.merge(puts, calls, on="symbol", how="inner")
+
+    # calculate iron condor values
+    iron_condors['max_profit'] = iron_condors['call_max_profit'] + iron_condors['put_max_profit']
+    iron_condors['theta'] = iron_condors['call_spread_theta'] + iron_condors['put_spread_theta']
+    iron_condors['bpr'] = spread_width * 100 - iron_condors['max_profit']
+    iron_condors['pop'] = iron_condors['call_pop'] * iron_condors['put_pop']
+    iron_condors['ev_pop'] = (iron_condors['max_profit'] * iron_condors['pop']) - ((1-iron_condors['pop']) * iron_condors['bpr'])
+    iron_condors['close'] = iron_condors['put_close']  # put and call close are the same
+    iron_condors['profit_to_bpr'] = iron_condors['max_profit'] / iron_condors['bpr']
+
+    # get symbol as column again and numric index of the df
+    iron_condors.reset_index(inplace=True)
+
+    # select which columns should be returned to streamlit view
+    streamlit_columns = [
+        'symbol',
+        'close',
+        'max_profit',
+        'profit_to_bpr',
+        'theta',
+        'bpr',
+        'pop',
+        'ev_pop',
+
+        # 'put_close',
+        # 'put_option_type',
+        'put_strike_sell',
+        # 'put_bid_sell',
+        # 'put_delta_sell',
+        # 'put_iv_sell',
+        'put_strike_buy',
+        # 'put_ask_buy',
+        # 'put_delta_buy',
+        # 'put_max_profit',
+        # 'put_bpr',
+        # 'put_profit_to_bpr',
+        # 'put_spread_theta',
+        # 'put_pop_delta',
+        # 'put_ev_pop_delta',
+        # 'put_pop',
+        # 'put_ev_pop',
+        # 'call_close',
+        # 'call_option_type',
+        'call_strike_sell',
+        # 'call_bid_sell',
+        # 'call_delta_sell',
+        # 'call_iv_sell',
+        'call_strike_buy',
+        # 'call_ask_buy',
+        # 'call_delta_buy',
+        # 'call_max_profit',
+        # 'call_bpr',
+        # 'call_profit_to_bpr',
+        # 'call_spread_theta',
+        # 'call_pop_delta',
+        # 'call_ev_pop_delta',
+        # 'call_pop',
+        # 'call_ev_pop',
+    ]
+
+    return iron_condors[streamlit_columns]
 
 
 if __name__ == "__main__":
@@ -61,10 +89,16 @@ if __name__ == "__main__":
     Keep the main for testing purposes
     """
 
+    import time
+
     df = pd.read_feather(PATH_DATAFRAME_DATA_MERGED_FEATHER)
-    expiration_date = '2025-03-14'
-    delta_target = 0.15
+    expiration_date = '2025-05-23'
+    delta_target = 0.2
     spread_width = 5
 
-    iron_condors_df = get_iron_condors(df, expiration_date, delta_target, spread_width)
-    pass
+    start = time.time()
+    iron_condors = get_iron_condors(df, expiration_date, delta_target, spread_width)
+    ende = time.time()
+
+    print(iron_condors.head())
+    print(f"Laufzeit: {ende - start:.6f} Sekunden")
