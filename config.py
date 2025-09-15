957b6b3f44657a1456795582192fb3ba53ddfaed
Degenhardt <daniel.degenhardt@sap.com>
2025-09-15 20:35:05 +0200

    Add all untracked files

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ PATH_ON_GOOGLE_DRIVE = "1ahLHST1IEUDf03TT3hEdbVm1r7rcxJcu"
 FILENAME_GOOGLE_DRIVE = FILENAME_MERGED_DATAFRAME
 #PATH_FOR_SERVICE_ACCOUNT_FILE = "service_account.json"
 
-PATH_FOR_SERVICE_ACCOUNT_FILE = r'C:\Python\google_upload2\service_account.json' 
+PATH_FOR_SERVICE_ACCOUNT_FILE = r'C:\Python\SKULD\service_account.json' 
 
 # FOLDERPATHS relative to main.py
 FOLDERPATHS = \
