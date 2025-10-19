4c79d3ad525f5a766e521a269d8f317e483ec529
Wuuzzaa <jonas-licht@gmx.de>
2025-10-19 16:52:39 +0200

    multifactor sql

--- src/multifactor_swingtrading_strategy.py
+++ src/multifactor_swingtrading_strategy.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from config import *
 from src.decorator_log_function import log_function
 
 @log_function
-def calculate_multifactor_swingtrading_strategy():
+def calculate_multifactor_swingtrading_strategy(df: pd.DataFrame) -> pd.DataFrame:
     pass
 
 if __name__ == "__main__":
