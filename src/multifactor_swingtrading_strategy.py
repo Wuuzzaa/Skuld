8fa91acad8665432f71fb044f16a7c3f783b9a66
Wuuzzaa <jonas-licht@gmx.de>
2025-10-19 10:36:29 +0200

    add multifactor_swingtrading.py and multifactor_swingtrading_strategy.py

---
+++ src/multifactor_swingtrading_strategy.py
@@ -0,0 +1,9 @@
+from config import *
+from src.decorator_log_function import log_function
+
+@log_function
+def calculate_multifactor_swingtrading_strategy():
+    pass
+
+if __name__ == "__main__":
+    pass
