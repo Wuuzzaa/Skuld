bc15dd5161e528e57be324703be47694c2fba8e8
Jakob Ingenfeld <jakob-ingenfeld@gmx.de>
2025-12-25 18:34:21 +0100

    Refactor database logging and add historization functionality
    - Replaced print statements with logger calls in database.py for better logging practices.
    - Introduced new functions to check for the existence of tables and views.
    - Added a new script (generate_history_views.py) to automate the creation of history views from existing SQL views.
    - Enhanced the HistorizationService to support daily, weekly, and monthly history tables, including the creation of necessary tables and views if they do not exist.
    - Updated the historization process to dynamically handle conflict keys based on table structure.
    - Improved SQL generation for history tables and views, ensuring proper column definitions and primary keys.

--- db/SQL/views/create_view/OptionData.sql
+++ db/SQL/views/create_view/OptionData.sql
@@ -1,54 +1,14 @@
 DROP VIEW IF EXISTS OptionData;
-
 CREATE VIEW
 	OptionData AS
 SELECT
-	-- OptionDataYahoo
-	a.symbol, 
-	a."option-type", 
-	DATE(a.expiration_date) as expiration_date, 
-	a.strike, 
-	a.bid, 
-	a.ask, 
-	a."contractSymbol",
-	a.currency,
-	a."lastPrice",
-	a.change,
-	a."percentChange",
-	a.option_open_interest,
-	a."contractSize",
-	a."lastTradeDate",
-	a."impliedVolatility",
-	a."inTheMoney",
-	a.option_volume,
-	-- OptionDataTradingView
-	CASE WHEN b.option_osi IS NOT NULL THEN TRUE ELSE FALSE END as has_option_data_tradingview,
-	b.delta,
-	b.gamma,
-	b.iv,
-	b.rho,
-	b."theoPrice",
-	b.theta,
-	b.vega,
-	b.option,
-	b.time,
-	b.exchange,
-	b.option_osi,
-	-- OptionPricingMetrics
-	CASE WHEN c.contractSymbol IS NOT NULL THEN TRUE ELSE FALSE END as has_option_pricing_metrics,
-	c.days_to_expiration,
-	c.premium_option_price,
-	c.spread,
-    c.spread_ptc,
-	c.intrinsic_value,
-	c.extrinsic_value,
-	c.strike_stock_price_difference,
-    c.strike_stock_price_difference_ptc
+	*
 FROM
-	OptionDataYahoo as a
-	LEFT OUTER JOIN OptionDataTradingView AS b ON a.contractSymbol = b.option_osi
-	-- b.symbol = b.symbol
-	-- and b.strike = b.strike
-	-- and b.expiration_date = b.expiration_date -- expiration_date in different formats 20250905 vs 2025-09-05 00:00:00.000000	
-	-- and b."option-type" = b."option-type" -- option-type in different formats call vs calls
-	LEFT OUTER JOIN OptionPricingMetrics as c ON a.contractSymbol = c.contractSymbol;
+	OptionDataHistory
+WHERE
+	date = (
+		SELECT
+			MAX(date)
+		FROM
+			OptionDataHistory
+	);
---
+++ db/SQL/views/create_view/history/StockDataHistory.sql
@@ -0,0 +1,53 @@
+DROP VIEW IF EXISTS StockDataHistory;
+CREATE VIEW
+    StockDataHistory AS
+SELECT
+    a.date,
+    a.symbol,
+    a.live_stock_price,
+    a.price_source,
+    a.live_price_timestamp,
+    CASE
+        WHEN b.symbol IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END as has_earnings_date,
+    b.earnings_date,
+    CAST(
+        julianday (b.earnings_date) - julianday (a.date) AS INTEGER
+    ) AS days_to_ernings,
+    CASE
+        WHEN c.symbol IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END as has_analyst_price_target,
+    c.analyst_mean_target,
+    -- Barchart Data
+    d.implied_volatility,
+    d.historical_volatility,
+    d.iv_percentile,
+    d.iv_rank,
+    d.iv_high,
+    d.iv_low,
+    d.put_call_vol_ratio,
+    d.put_call_oi_ratio,
+    d.todays_volume,
+    d.volume_avg_30d,
+    d.todays_open_interest,
+    d.open_int_30d
+FROM
+    StockPriceHistory AS a
+    LEFT OUTER JOIN (
+        SELECT
+            date,
+            symbol,
+            CASE
+                WHEN earnings_date LIKE '%.%.%' THEN substr (earnings_date, 7, 4) || '-' || substr (earnings_date, 4, 2) || '-' || substr (earnings_date, 1, 2)
+                else NULL
+            end as earnings_date
+        FROM
+            EarningDatesHistory
+    ) AS b ON a.symbol = b.symbol
+    AND a.date = b.date
+    LEFT OUTER JOIN AnalystPriceTargetsHistory AS c ON a.symbol = c.symbol
+    AND a.date = c.date
+    LEFT OUTER JOIN StockDataBarchartHistory AS d ON a.symbol = d.symbol
+    AND a.date = d.date;
