0ba8395e994d92239a6c3f1efe57ddac73cda753
Degenhardt <daniel.degenhardt@sap.com>
2026-01-05 22:09:41 +0100

    Add Massive API keys to docker-compose and config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ from src.get_version import get_version
 # Massiv API
 load_dotenv()
 MASSIVE_API_KEY = os.getenv('MASSIVE_API_KEY')
+MASSIVE_API_KEY_FLAT_FILES = os.getenv('MASSIVE_API_KEY_FLAT_FILES')
 TEST_KEY= os.getenv('TEST_KEY') # DEBUG
 
 # Base path relative to base folder
