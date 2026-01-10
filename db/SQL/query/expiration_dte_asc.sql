4d7e96aec325fc02a97af814270286c7d507eaec
Wuuzzaa <jonas-licht@gmx.de>
2026-01-10 21:03:18 +0100

    Bugfix ValueError: handle NaN in days_to_expiration by casting to int only if not NaN

--- db/SQL/query/expiration_dte_asc.sql
+++ db/SQL/query/expiration_dte_asc.sql
@@ -3,5 +3,7 @@ SELECT DISTINCT
     days_to_expiration
 FROM
     OptionData
+WHERE
+    days_to_expiration is not null -- todo db scheme anpassen nur massive api optionsdaten. inkl. der berechneten Felder wie dte.
 ORDER BY
     days_to_expiration;
