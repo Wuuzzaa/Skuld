ee335226fb200bf0b73be7161312635e4fa9841d
Wuuzzaa <jonas-licht@gmx.de>
2026-02-21 11:57:27 +0100

    Add `iv_low`, `iv_high`, and `Beta` columns to `symbolpage.sql` query

--- db/SQL/query/symbolpage.sql
+++ db/SQL/query/symbolpage.sql
@@ -5,8 +5,11 @@ SELECT DISTINCT
     ROUND("Current-Div"::numeric, 2) AS "Current Dividend",
     "Classification",
     ROUND(iv::numeric, 2) AS iv,
+    ROUND(iv_low::numeric, 2) AS iv_low,
+    ROUND(iv_high::numeric, 2) AS iv_high,
     ROUND(iv_rank::numeric, 2) AS iv_rank,
     ROUND(iv_percentile::numeric, 2) AS iv_percentile,
-    ROUND(historical_volatility_30d::numeric, 2) AS historical_volatility_30d
+    ROUND(historical_volatility_30d::numeric, 2) AS historical_volatility_30d,
+    ROUND("KeyStats_beta"::numeric, 2) AS Beta
 FROM "OptionDataMerged"
 WHERE symbol = :symbol;
