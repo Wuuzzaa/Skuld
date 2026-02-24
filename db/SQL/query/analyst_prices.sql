b7902827145b436cc9301abb526a0bf882771861
Wuuzzaa <jonas-licht@gmx.de>
2026-02-24 22:36:22 +0100

    Comment out unused recommendation-related columns in `analyst_prices.py` to simplify the dataset.

--- db/SQL/query/analyst_prices.sql
+++ db/SQL/query/analyst_prices.sql
@@ -2,8 +2,8 @@ SELECT DISTINCT
     symbol,
     live_stock_price as close,
     analyst_mean_target,
-    NULL recommendation,
-    NULL "Recommend.All",
+    -- NULL recommendation, -- replace later with own recommendation based on technical indicators
+    -- NULL "Recommend.All", -- replace later with own recommendation based on technical indicators
     "target-close$",
     "target-close%"
 FROM
