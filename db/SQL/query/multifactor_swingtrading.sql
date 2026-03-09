323a2bf223c78987df89eeb23ccddcbdfe0179e8
Wuuzzaa <jonas-licht@gmx.de>
2026-03-09 14:31:02 +0100

    Add additional financial metrics to `multifactor_swingtrading.sql` query

--- db/SQL/query/multifactor_swingtrading.sql
+++ db/SQL/query/multifactor_swingtrading.sql
@@ -1,12 +1,18 @@
 SELECT DISTINCT
     symbol,
+    live_stock_price                                                            AS price,
     "KeyStats_priceToBook"                                                      AS price_to_book,
     "Summary_trailingPE"                                                        AS price_to_earnings,
     "Summary_priceToSalesTrailing12Months"                                      AS price_to_sales,
     "EBITDA" / NULLIF("EnterpriseValue", 0)                                     AS ebitda_to_enterprise_value,
     LIVE_STOCK_PRICE / NULLIF(("FreeCashFlow" / NULLIF("KeyStats_sharesOutstanding", 0)), 0) AS price_to_cashflow,
     "Summary_trailingAnnualDividendYield"                                       AS shareholder_yield,
-    "KeyStats_52WeekChange"                                                     AS "1_year_price_appreciation"
+    "KeyStats_52WeekChange"                                                     AS "1_year_price_appreciation",
+    "BasicEPS",
+    "DilutedEPS",
+    "Forward_EPS_Growth_Percent",
+    "KeyStats_trailingEps",
+    "KeyStats_forwardEps"
 FROM
     "OptionDataMerged"
 WHERE
