e74a8cf63dcd1deb825fff0b9fa95d3dfa50181d
Jakob Ingenfeld <jakob-ingenfeld@gmx.de>
2026-03-17 09:04:57 +0100

    Optimize analyst prices

--- db/SQL/query/analyst_prices.sql
+++ db/SQL/query/analyst_prices.sql
@@ -1,10 +1,11 @@
 SELECT
     symbol,
-    live_stock_price as close,
+    live_stock_price AS close,
     analyst_mean_target,
     -- NULL recommendation, -- replace later with own recommendation based on technical indicators
     -- NULL "Recommend.All", -- replace later with own recommendation based on technical indicators
     "target-close$",
     "target-close%"
 FROM
-    "StockData";
+    "StockData"
+WHERE analyst_mean_target IS NOT NULL;
