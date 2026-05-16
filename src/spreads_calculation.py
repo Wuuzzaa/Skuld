f211bc58e9b03d8fa50d5ddf5f50da3a5d5d0b42
Wuuzzaa <jonas-licht@gmx.de>
2026-05-16 11:15:21 +0200

    Refactor spreads calculation: update query logic for enhanced filtering, add support for new parameters like strategy type, IV rank, and day volume, and improve output with expanded option details.

--- src/spreads_calculation.py
+++ src/spreads_calculation.py
@@ -185,106 +185,124 @@ if __name__ == "__main__":
     logger.info(f"Start {__name__} ({__file__})")
 
     params = {
-        "expiration_date": "2026-02-20",
+        "expiration_date": "2026-07-17",
         "option_type": "put",
         "delta_target": 0.2,
         "spread_width": 5,
-        "min_open_interest": 100
+        "min_open_interest": 0,
+        "min_day_volume": 0,
+        "min_iv_rank": 0,
+        "min_iv_percentile":0,
+        "strategy_type": "credit",
     }
 
 
     # test query. ensure to use the running query in the production code as well :D
     sql_query = """
     WITH FilteredOptions AS (
-        SELECT
-            symbol,
-            expiration_date,
-            contract_type AS option_type,
-            strike_price AS strike,
-            day_close AS last_option_price,
-            abs(greeks_delta) AS delta,
-            implied_volatility AS iv,
-            greeks_theta AS theta,
-            close,
-            earnings_date,
-            days_to_expiration,
-            days_to_earnings,
-            open_interest AS option_open_interest,
-            expected_move,
-            ROW_NUMBER() OVER (PARTITION BY symbol ORDER BY abs(greeks_delta) DESC) AS row_num,
-            analyst_mean_target,
-            recommendation
-        FROM
-            "OptionDataMerged"
-        WHERE
-            expiration_date = :expiration_date
-            AND contract_type = :option_type
-            AND abs(greeks_delta) <= :delta_target
-            AND open_interest >= :min_open_interest
-    ),
-    
-    SelectedSellOptions AS (
-        SELECT
-            symbol,
-            strike AS sell_strike,
-            expiration_date,
-            option_type,
-            last_option_price AS sell_last_option_price,
-            delta AS sell_delta,
-            iv AS sell_iv,
-            theta AS sell_theta,
-            close AS sell_close,
-            earnings_date,
-            days_to_expiration,
-            days_to_earnings,
-            option_open_interest AS sell_open_interest,
-            expected_move AS sell_expected_move,
-            analyst_mean_target,
-            recommendation
-        FROM
-            FilteredOptions
-        WHERE
-            row_num = 1
-    )
-    
-    --spread data
     SELECT
-        -- sell option
-        sell.symbol,
-        sell.expiration_date,
-        sell.option_type,
-        sell.sell_close AS close,
-        sell.earnings_date,
-        sell.days_to_expiration,
-        sell.days_to_earnings,
-        sell.sell_strike,
-        sell.sell_last_option_price,
-        sell.sell_delta,
-        sell.sell_iv,
-        sell.sell_theta,
-        sell.sell_open_interest,
-        sell.sell_expected_move,
-        sell.analyst_mean_target,
-        sell.recommendation,
-        -- buy option
-        buy.strike               AS buy_strike,
-        buy.last_option_price    AS buy_last_option_price,
-        buy.delta                AS buy_delta,
-        buy.iv                   AS buy_iv,
-        buy.theta                AS buy_theta,
-        buy.option_open_interest AS buy_open_interest,
-        buy.expected_move        AS buy_expected_move
+        symbol,
+        expiration_date,
+        contract_type AS option_type,
+        strike_price AS strike,
+        day_close AS last_option_price,
+        abs(greeks_delta) AS delta,
+        implied_volatility AS iv,
+        greeks_theta AS theta,
+        LIVE_STOCK_PRICE AS close,
+        earnings_date,
+        days_to_expiration,
+        days_to_earnings,
+        open_interest AS option_open_interest,
+        expected_move,
+        analyst_mean_target,
+        day_volume,
+        company_name,
+        company_industry,
+        company_sector,
+        historical_volatility_30d,
+        iv_rank,
+        iv_percentile
     FROM
-        SelectedSellOptions sell
-    INNER JOIN
-        FilteredOptions buy
-        ON sell.symbol = buy.symbol
-        AND buy.strike = (
-            CASE
-                WHEN sell.option_type = 'put' THEN sell.sell_strike - :spread_width
-                WHEN sell.option_type = 'call' THEN sell.sell_strike + :spread_width
-            END
-        );
+        "OptionDataMerged"
+    WHERE
+        open_interest >= :min_open_interest
+        AND day_volume >= :min_day_volume
+        AND iv_rank >= :min_iv_rank
+        AND iv_percentile >= :min_iv_percentile
+),
+
+TargetOptions AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY symbol, expiration_date, option_type
+            ORDER BY abs(delta - :delta_target) ASC
+        ) as delta_rank
+    FROM
+        FilteredOptions
+    WHERE
+        expiration_date = :expiration_date
+        AND option_type = :option_type
+)
+
+SELECT
+    -- symbol data
+    sell.symbol,
+    sell.expiration_date,
+    sell.option_type,
+    sell.close,
+    sell.earnings_date,
+    sell.company_name AS "Company",
+    sell.days_to_expiration,
+    sell.days_to_earnings,
+    sell.analyst_mean_target,
+    sell.company_industry,
+    sell.company_sector,
+    sell.historical_volatility_30d,
+    sell.iv_rank,
+    sell.iv_percentile,
+    -- sell option
+    sell.strike AS sell_strike,
+    sell.last_option_price AS sell_last_option_price,
+    sell.delta AS sell_delta,
+    sell.iv AS sell_iv,
+    sell.theta AS sell_theta,
+    sell.option_open_interest AS sell_open_interest,
+    sell.expected_move AS sell_expected_move,
+    sell.day_volume AS sell_day_volume,
+    -- buy option
+    buy.strike AS buy_strike,
+    buy.last_option_price AS buy_last_option_price,
+    buy.delta AS buy_delta,
+    buy.iv AS buy_iv,
+    buy.theta AS buy_theta,
+    buy.option_open_interest AS buy_open_interest,
+    buy.expected_move AS buy_expected_move,
+    buy.day_volume AS buy_day_volume
+FROM
+    TargetOptions sell
+INNER JOIN
+    FilteredOptions buy
+    ON sell.symbol = buy.symbol
+    AND sell.expiration_date = buy.expiration_date
+    AND sell.option_type = buy.option_type
+    AND buy.strike = (
+        CASE
+            WHEN :strategy_type = 'credit' THEN 
+                CASE
+                    WHEN sell.option_type = 'put' THEN sell.strike - :spread_width
+                    WHEN sell.option_type = 'call' THEN sell.strike + :spread_width
+                END
+            WHEN :strategy_type = 'debit' THEN
+                CASE
+                    WHEN sell.option_type = 'put' THEN sell.strike + :spread_width
+                    WHEN sell.option_type = 'call' THEN sell.strike - :spread_width
+                END
+        END
+    )
+WHERE
+    sell.delta_rank = 1;
 
     """
 
